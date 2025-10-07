f25171125cf84569f2aac8499c800879234b9fc1
Haripriyavelu <haripriyaveluchamy@aity.dev>
2025-10-07 19:28:07 +0530

    added terraform state file

---
+++ terraform/backend.tf
@@ -0,0 +1,21 @@
+# backend.tf
+terraform {
+  backend "azurerm" {
+    # Empty backend configuration - will be filled via CLI parameters
+  }
+  
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.75.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
